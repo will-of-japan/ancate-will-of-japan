a006a447ce870cd182eae1c7ef568e5ca189f186
will-of-japan <willofjapan.community.unei@gmail.com>
2026-03-18 20:18:11 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  API_BASE: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',
+  API_BASE: 'https://script.google.com/macros/s/AKfycbwXZGr7ZeMDfHTkKLy5uSwhEJP0bUiI-_BLlPNz8jUtHWDUGcN9dQlaCfoB0c-qQD7X/exec',
 };
